381f770bdfd5a90e5ebf299f782399632b302020
Adam <chillcoders@gmail.com>
2022-02-18 18:36:51 -0500

    head-nthings

---
+++ components/Layout.js
@@ -0,0 +1,18 @@
+export default function Layout(title, keywords, description, children) {
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+
+        <meta name="keywords" content={keywords} />
+      </Head>
+      {children}
+    </div>
+  );
+}
+Layout.defaultProps = {
+  title: "Adams Adjacent Events| Find the most fulfilling events",
+  description: "Find events that truly make you content",
+  keywords: "music, art, meditation, writing, yoga, food, drinks",
+};
