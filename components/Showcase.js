b2d7bdb42bf0aebde4e630e3b739507a35fd772b
Adam <chillcoders@gmail.com>
2022-02-21 14:41:03 -0500

    showcase

---
+++ components/Showcase.js
@@ -0,0 +1,9 @@
+import styles from "@/styles/Showcase.module.css";
+export default function Showcase() {
+  return (
+    <div className={styles.showcase}>
+      <h1>Adam's Adjacent Event Highlights</h1>
+      <h2>The cream of the crop</h2>
+    </div>
+  );
+}
